1cabb0ba60c27234ac13d0bb580cc6c7cd9235dc
atharv-khede <atharvkhede625@gmail.com>
2023-10-11 16:20:14 +0530

    changed host settings

--- operations/wsgi.py
+++ operations/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 settings_module = 'operations.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'operations.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-
+  
 application = get_wsgi_application()
