1c4fd80cfb90cbd8c0dfbab58ca0de431087eb2a
atharv-khede <atharvkhede625@gmail.com>
2023-10-11 16:22:15 +0530

    update migrations

--- content/migrations/0002_rename_mydata_project.py
+++ content/migrations/0002_rename_mydata_project.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RenameModel(
-            old_name='Mydata',
+            old_name='Mydata', 
             new_name='Project',
         ),
     ]
